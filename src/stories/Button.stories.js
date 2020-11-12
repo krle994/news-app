c72006786185dc7fcb78215317415b4e9d7cc176
Milan Krstic <krlekrstic018@gmail.com>
2020-11-12 23:03:36 +0100

    Remove example from storybook

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,36 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import { Button } from './Button';
+import Button from "../components/Button";
 
 export default {
-  title: 'Example/Button',
-  component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    title: "Button",
+    component: Button,
 };
 
-const Template = (args) => <Button {...args} />;
-
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+export const Link = () => (
+    <Button link="true" to="/">
+        Link
+    </Button>
+);
